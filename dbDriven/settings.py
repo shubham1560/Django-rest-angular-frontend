c041e3c0ef5c32d432990de8ec44e6608b607d40
Shubham <shubhamsinha2050@gmail.com>
2019-08-30 08:10:07 +0530

    Resolved the error of database by adding attribute in the migration command

--- dbDriven/settings.py
+++ dbDriven/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from jobs.apps import JobsConfig
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,3 +125,4 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
--- jobs/models.py
+++ jobs/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 
-class Jobs(models.Model):
+class Job(models.Model):
     image = models.ImageField(upload_to="images/")
     summary = models.CharField(max_length=200)
