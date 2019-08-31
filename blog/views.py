ee18645164688e8dd850e79129760e9f7573d45f
Shubham <shubhamsinha2050@gmail.com>
2019-08-31 10:56:22 +0530

    Gonna turn the project into API and try to get on with angular as front-end

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
 
 
 def bloghome(request):
-    return render(request, 'index.html')
+    blogs = Blog.objects
+    print(blogs)
+    return render(request, 'blog\\index.html', {'blogs': blogs})
