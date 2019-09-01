83f704ce5bb93ba68719df726ddb77618996e9f9
Shubham <shubhamsinha2050@gmail.com>
2019-09-01 21:07:49 +0530

    Added first Angular Html Page jobsFromApi.html

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,8 +1,10 @@
 from .api import JobApi
 from django.urls import path
 import jobs.views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', jobs.views.home, name="jobs"),
-    path('jobs', JobApi.as_view())
+    path('jobs', JobApi.as_view()),
+    path('home', TemplateView.as_view(template_name = "job/jobsFromApi.html")),
 ]
