9a938abc999465abe73f976b45539fb087b55cd6
Shubham <shubhamsinha2050@gmail.com>
2019-09-01 23:58:03 +0530

    Made a call and got the data from job's django rest api

--- jobs/static/jobs.js
+++ jobs/static/jobs.js
@@ -3,15 +3,38 @@
 
     var app = angular.module('jobsApi', []);
 
-    app.controller('jobsApiController', function jobs($scope) {
+    app.controller('jobsApiController', function($scope) {
         $scope.name = "Shubham";
     });
 
-    app.controller('demoStack', function jobsController($scope) {
+    app.controller('demoStack', function($scope) {
         $scope.person = {
             name: "Shubham",
             age: 23,
         }
-        console.log($scope.person);
+    });
+
+    app.controller('dataJobApi', function($scope, $http) {
+        $scope.search = function() {
+            $http.get("http://127.0.0.1:8000/jobs/jobs").then(function(response) {
+                console.log(response);
+
+            })
+        }
+    });
+
+    app.controller("userName", function($rootScope, $scope, $http) {
+        $scope.userName = "shubham1560";
+        $scope.search = function(username) {
+            $http.get("https://api.github.com/users/" + username).then(function(response) {
+                console.log(response.data);
+                $scope.userData = response.data;
+                $http.get(response.data.repos_url).then(function(response) {
+                    $scope.repos = response.data;
+                })
+            }, function(reason) {
+                $scope.error = "No user with this name";
+            });
+        }
     });
 }());
