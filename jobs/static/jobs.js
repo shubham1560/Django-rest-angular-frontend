593b2ebb9549ed2353928e98fe723db6822b8c49
Shubham <shubhamsinha2050@gmail.com>
2019-09-04 23:13:28 +0530

    figouring out how to post file type data in angular js

--- jobs/static/jobs.js
+++ jobs/static/jobs.js
@@ -18,8 +18,31 @@
         $scope.search = function() {
             $http.get("/jobs/jobs").then(function(response) {
                 $scope.data = response.data;
-                console.log(response.data);
+                console.log(response);
             })
+        }
+        $scope.formData = function() {
+            console.log("Inside");
+            console.log($scope.test);
+            console.log($scope.file);
+        }
+
+        console.log($scope.pd);
+        $scope.insert = function() {
+            // var req = {
+            //     method: 'POST',
+            //     url: '/jobs/jobs',
+            //     headers: {
+            //         'Content-Type': undefined
+            //     },
+            //     $scope.data: { image: 'test', summary: 'Well' }
+            // }
+
+            $http.post("/jobs/jobs/", { image: "", summary: 'Post Test' }).then(function() {
+                console.log("Posted Data");
+            }, function() {
+                console.log("Couldn't post data");
+            });
         }
     });
 }())
