16bbf425db34b83ebf12a374f29eff3fe2e97c35
Shubham <shubhamsinha2050@gmail.com>
2019-09-05 22:50:54 +0530

    Started to work on a simple application to first figure out the angular js interaction with the database

--- dbDriven/settings.py
+++ dbDriven/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'jobs.apps.JobsConfig',
     'blog.apps.BlogConfig',
+    'basic_api.apps.BasicApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
