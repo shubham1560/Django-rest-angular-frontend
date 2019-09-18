0a1377c4d5541a1a012c077dd20761f2ced4f6e3
Shubham <shubhamsinha2050@gmail.com>
2019-09-18 23:16:05 +0530

    gonna make a branch of it to test new feature

--- basic_api/models.py
+++ basic_api/models.py
@@ -7,6 +7,9 @@ class Summary(models.Model):
     summary = models.CharField(max_length=400)
     tech = models.CharField(max_length=100)
 
+    def well(self):
+        return self.summary + self.tech
+
     def __str__(self):
-        return self.summary and self.tech
+        return self.summary + self.tech + self.well()
 
